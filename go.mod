e2b5259de11a1062391b0437f649b33d429e9602
Igor Kuznetsov <igk@igk.ru>
2019-11-01 10:25:32 +0300

    Add test

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/klauspost/cpuid v1.2.1 // indirect
 	github.com/mholt/certmagic v0.8.3
 	github.com/miekg/dns v1.1.22 // indirect
+	github.com/stretchr/testify v1.4.0
 	golang.org/x/crypto v0.0.0-20191029031824-8986dd9e96cf // indirect
 	golang.org/x/net v0.0.0-20191028085509-fe3aa8a45271 // indirect
 	golang.org/x/sys v0.0.0-20191029155521-f43be2a4598c // indirect
--- s3storage_test.go
+++ s3storage_test.go
@@ -1,1 +1,159 @@
 package magicstorage
+
+import (
+	"path"
+	"testing"
+	"time"
+
+	"github.com/mholt/certmagic"
+	"github.com/stretchr/testify/assert"
+)
+
+const testBacket = "s3tlstest"
+const testRegion = "us-east-1"
+
+// these tests needs a running S3 server
+func setupS3Env(t *testing.T) *S3Storage {
+
+	cs, err := NewS3Storage(testBacket, testRegion)
+	assert.NoError(t, err)
+
+	// _, err = cs.SVC.DeleteTree
+	// assert.NoError(t, err)
+	return cs
+}
+
+func TestS3Storage_Store(t *testing.T) {
+	cs := setupS3Env(t)
+
+	err := cs.Store(path.Join("acme", "example.com", "sites", "example.com", "example.com.crt"), []byte("crt data"))
+	assert.NoError(t, err)
+}
+
+func TestS3Storage_Exists(t *testing.T) {
+	cs := setupS3Env(t)
+
+	key := path.Join("acme", "example.com", "sites", "example.com", "example.com.crt")
+
+	err := cs.Store(key, []byte("crt data"))
+	assert.NoError(t, err)
+
+	exists := cs.Exists(key)
+	assert.True(t, exists)
+}
+
+func TestS3Storage_Load(t *testing.T) {
+	cs := setupS3Env(t)
+
+	key := path.Join("acme", "example.com", "sites", "example.com", "example.com.crt")
+	content := []byte("crt data")
+
+	err := cs.Store(key, content)
+	assert.NoError(t, err)
+
+	contentLoded, err := cs.Load(key)
+	assert.NoError(t, err)
+
+	assert.Equal(t, content, contentLoded)
+}
+
+func TestS3Storage_Delete(t *testing.T) {
+	cs := setupS3Env(t)
+
+	key := path.Join("acme", "example.com", "sites", "example.com", "example.com.crt")
+	content := []byte("crt data")
+
+	err := cs.Store(key, content)
+	assert.NoError(t, err)
+
+	err = cs.Delete(key)
+	assert.NoError(t, err)
+
+	exists := cs.Exists(key)
+	assert.False(t, exists)
+
+	contentLoaded, err := cs.Load(key)
+	assert.Nil(t, contentLoaded)
+
+	_, ok := err.(certmagic.ErrNotExist)
+	assert.True(t, ok)
+}
+
+func TestS3Storage_Stat(t *testing.T) {
+	cs := setupS3Env(t)
+
+	key := path.Join("acme", "example.com", "sites", "example.com", "example.com.crt")
+	content := []byte("crt data")
+
+	err := cs.Store(key, content)
+	assert.NoError(t, err)
+
+	info, err := cs.Stat(key)
+	assert.NoError(t, err)
+
+	assert.Equal(t, key, info.Key)
+}
+
+func TestS3Storage_List(t *testing.T) {
+	cs := setupS3Env(t)
+
+	err := cs.Store(path.Join("acme", "example.com", "sites", "example.com", "example.com.crt"), []byte("crt"))
+	assert.NoError(t, err)
+	err = cs.Store(path.Join("acme", "example.com", "sites", "example.com", "example.com.key"), []byte("key"))
+	assert.NoError(t, err)
+	err = cs.Store(path.Join("acme", "example.com", "sites", "example.com", "example.com.json"), []byte("meta"))
+	assert.NoError(t, err)
+
+	keys, err := cs.List(path.Join("acme", "example.com", "sites", "example.com"), true)
+	assert.NoError(t, err)
+	assert.Len(t, keys, 3)
+	assert.Contains(t, keys, path.Join("acme", "example.com", "sites", "example.com", "example.com.crt"))
+}
+
+func TestS3Storage_ListNonRecursive(t *testing.T) {
+	cs := setupS3Env(t)
+
+	err := cs.Store(path.Join("acme", "example.com", "sites", "example.com", "example.com.crt"), []byte("crt"))
+	assert.NoError(t, err)
+	err = cs.Store(path.Join("acme", "example.com", "sites", "example.com", "example.com.key"), []byte("key"))
+	assert.NoError(t, err)
+	err = cs.Store(path.Join("acme", "example.com", "sites", "example.com", "example.com.json"), []byte("meta"))
+	assert.NoError(t, err)
+
+	keys, err := cs.List(path.Join("acme", "example.com", "sites"), false)
+	assert.NoError(t, err)
+
+	assert.Len(t, keys, 1)
+	assert.Contains(t, keys, path.Join("acme", "example.com", "sites", "example.com"))
+}
+
+func TestS3Storage_LockUnlock(t *testing.T) {
+	cs := setupS3Env(t)
+	lockKey := path.Join("acme", "example.com", "sites", "example.com", "lock")
+
+	err := cs.Lock(lockKey)
+	assert.NoError(t, err)
+
+	err = cs.Unlock(lockKey)
+	assert.NoError(t, err)
+}
+
+func TestS3Storage_TwoLocks(t *testing.T) {
+	cs := setupS3Env(t)
+	cs2 := setupS3Env(t)
+	lockKey := path.Join("acme", "example.com", "sites", "example.com", "lock")
+
+	err := cs.Lock(lockKey)
+	assert.NoError(t, err)
+
+	go time.AfterFunc(5*time.Second, func() {
+		err = cs.Unlock(lockKey)
+		assert.NoError(t, err)
+	})
+
+	err = cs2.Lock(lockKey)
+	assert.NoError(t, err)
+
+	err = cs2.Unlock(lockKey)
+	assert.NoError(t, err)
+}
